74d0c8049e7594a92f02bf35ed207a10dada3209
CaesarMaster <dereza.niki@gmail.com>
2022-11-12 13:54:13 +0100

    Lesson 76

--- Python module 11/Lesson 76. files/tour.py
+++ Python module 11/Lesson 76. files/tour.py
@@ -9,9 +9,10 @@ with open(os.path.abspath('first_tour.txt'), encoding='UTF-8', mode='r') as file
     for i in strings:
         n = i.split()
         scores[f'{n[0]}  {n[1]}'] = int(n[2])
-    print(scores)
-
-    for i in scores:
+    scores_list = sorted(scores, key=lambda a: scores[a], reverse=True)
+    results = []
+    for j, i in enumerate(scores_list):
         if scores[i] > 80:
             surname, name = i.split()
-            file2.write('\n'f'{name[0]}. {surname} {scores[i]}')
+            results.append(f'{j + 1}) {name[0]}. {surname} {scores[i]}')
+    file2.write('\n'.join(results))
