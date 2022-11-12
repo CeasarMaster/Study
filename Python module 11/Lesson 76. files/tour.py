e07c427c7abeaaa84804a75ba9507198df34bf0c
CaesarMaster <dereza.niki@gmail.com>
2022-11-12 13:21:35 +0100

    Lesson 76

--- Python module 11/Lesson 76. files/tour.py
+++ Python module 11/Lesson 76. files/tour.py
@@ -1,12 +1,17 @@
 import os
 
-with open(os.path.abspath('first_tour.txt'), encoding='UTF-8', mode='r') as file:
+with open(os.path.abspath('first_tour.txt'), encoding='UTF-8', mode='r') as file, open(
+        os.path.abspath('second_tour.txt'), encoding='UTF-8', mode='w') as file2:
     strings = file.read().split('\n')
     pass_score = int(strings.pop(0))
-    scores={}
+    scores = {}
 
     for i in strings:
-        n=i.split()
-        scores[n[0]+n[1]]=n[2]
+        n = i.split()
+        scores[f'{n[0]}  {n[1]}'] = int(n[2])
     print(scores)
 
+    for i in scores:
+        if scores[i] > 80:
+            surname, name = i.split()
+            file2.write('\n'f'{name[0]}. {surname} {scores[i]}')
