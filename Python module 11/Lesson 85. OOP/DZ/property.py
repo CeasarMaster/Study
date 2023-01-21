a50fc74e9009b569290b46de828f815e8fe35fa2
CaesarMaster <dereza.niki@gmail.com>
2023-01-21 12:32:53 +0100

    OOP Update

---
+++ Python module 11/Lesson 85. OOP/DZ/property.py
@@ -0,0 +1,21 @@
+class Property:
+
+    def __init__(self, worth):
+        self.worth = worth
+
+    def calc_taxes(self):
+        pass
+
+
+class Apartment(Property):
+    pass
+
+
+class Car(Property):
+    pass
+
+
+class CountryHouse(Property):
+    pass
+
+
