bf86668b55d64168bfc0eae9e3280078565100da
CaesarMaster <dereza.niki@gmail.com>
2023-02-11 14:06:13 +0100

    Escape The Blaze Update

--- Projects/Personal Project- Text Quest Development/Main Program/text_quest.py
+++ Projects/Personal Project- Text Quest Development/Main Program/text_quest.py
@@ -1,15 +1,40 @@
 import tkinter
+import time
+from PIL import ImageTk, Image
 
+
+def print_data(event):
+    flag = False
+    label['text'] = f''
+    button_start.configure(state='disabled')
+    for i in data:
+        time.sleep(0.06)
+        label['text'] += f' {i}'
+        window.update()
+    button_start.configure(state='active')
+
+
+def start_game():
+    window.title('Game started')
+
+
+flag = True
+data = 'You play as a resident of a small town surrounded by dense forests,\n\n who suddenly finds themselves in the middle of a raging wildfire.\n\n Your goal is to escape the blaze and reach safety.'
 window = tkinter.Tk()
-window.title('Wildfires Text Quest')
+window.title('Escape The Blaze')
 window.geometry('1920x1080')
 window.resizable(True, True)
+window.configure(background='black')
+label = tkinter.Label(window,
+                      text='Click anywhere to begin',
+                      background='black', foreground='white', font=('Arial', 14))
+label.pack()
+label.place(anchor='center', relx=0.5, rely=0.5)
 
-button_start = tkinter.Button(window, text='START GAME')
-button_start.place(x=0, y=0, width=100, height=40)
-button_controls = tkinter.Button(window, text='CONTROLS')
-button_controls.place(x=100, y=0, width=100, height=40)
-button_manual = tkinter.Button(window, text='MANUAL')
-button_manual.place(x=200, y=0, width=100, height=40)
-
+button_start = tkinter.Button(window, text='START GAME', background='black', foreground='white', font=('Arial', 14),
+                              command=start_game)
+button_start.pack()
+button_start.place(anchor='center', width=150, height=80, relx=0.5, rely=0.8)
+if flag:
+    window.bind('<ButtonPress>', print_data)
 window.mainloop()
--- Python module 11/Lesson 87. OOP/data_analyst.py
+++ Python module 11/Lesson 87. OOP/data_analyst.py
@@ -1,34 +1,44 @@
 import os
 
-rus_alpha = [chr(i) for i in range(ord('А'), ord('Я') + 1)]
-words = []
-
 
 class DataAnalyst:
     def __init__(self, text: str):
         self.text = text
+        self.rus_alpha = [chr(i) for i in range(ord('А'), ord('Я') + 1)]
 
     def is_kirillik(self, word):
-        for i in rus_alpha:
-            print(i)
-
-            if i in word:
-                return True
-            else:
-                return False
+        for i in word.upper():
+            if i.isalpha():
+                if i not in self.rus_alpha:
+                    return False
+        return True
 
     def edit_text(self):
         with open(os.path.abspath('text.txt'), encoding='UTF-8', mode='r') as file:
-            for i in file:
-                for x in i:
-                    if x.isalpha():
-                        words.append(x)
+            file = file.read()
+            words = []
+            word = ''
+            for i_sym in file:
+                if i_sym.isalpha():
+                    word += i_sym
+                else:
+                    if word != '':
+                        words.append(word)
+                        word = ''
+            return words
 
     def lexic(self):
-        pass
+        lex = text.edit_text()
+        count = len(lex)  # All words in list
+        unique = 0
+        for i in lex:
+            if lex.count(i) == 1:
+                unique += 1
+        diff = unique / count
+        return diff
 
 
-text = DataAnalyst('')
+text = DataAnalyst('мама мыла раму, мама мыла Рому и ковер')
 print(text.is_kirillik('Прив'))
-text.edit_text()
-print(words)
+print(text.edit_text())
+print(text.lexic())
