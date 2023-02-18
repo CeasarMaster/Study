07b17c8a19afe094a49f7f02896d8728bd168903
CaesarMaster <dereza.niki@gmail.com>
2023-02-18 13:20:38 +0100

    Escape The Blaze Update

--- Projects/Personal Project- Text Quest Development/Main Program/text_quest.py
+++ Projects/Personal Project- Text Quest Development/Main Program/text_quest.py
@@ -1,13 +1,14 @@
 import tkinter
 import time
 
+from PIL import ImageTk, Image
 
 
 def walking_text(data, font='Arial', size=14):
     label['text'] = f''
     label['font'] = (font, size)
     for i in data:
-        time.sleep(0.04 )
+        time.sleep(0.04)
         label['text'] += f' {i}'
         window.update()
 
@@ -31,8 +32,14 @@ def start_game():
 def main_game():
     label.destroy()
     text_history = tkinter.Text(window, state='disabled', bg='black', fg='white', font=('Arial', 14))
+    text_entry = tkinter.Entry(window, width=230)
+    text_entry.pack()
+    text_entry.place(x=300, y=850, relheight=0.07)
     text_history.pack()
     text_history.place(x=0, y=0, relheight=0.75, relwidth=1)
+    send = tkinter.Button(window, text='SEND', background='black', foreground='white', font=('Arial', 14))
+    send.pack()
+    send.place(x=1730, y=900)
     label_health = tkinter.Label(window, text='Health:', bg='black', fg='white', font=('Arial', 14))
     display_health = tkinter.Label(window, text='<==========>', bg='black', fg='white', font=('Arial', 14))
     label_hunger = tkinter.Label(window, text='Hunger:', bg='black', fg='white', font=('Arial', 14))
@@ -48,8 +55,13 @@ def main_game():
 
 
 window = tkinter.Tk()
+window_height = window.winfo_height()
+window_width = window.winfo_width()
+print(window_width)
+print(window_height)
 window.title('Escape The Blaze')
-window.geometry('1920x1080')
+window.attributes('-fullscreen', True)
+#window.geometry('1920x1080')
 window.resizable(True, True)
 window.configure(background='black')
 label = tkinter.Label(window,
