cde82b1b95c9549a83046c1b0fafe3d5504b7a32
CaesarMaster <dereza.niki@gmail.com>
2023-04-25 22:43:04 +0200

    MRO

--- Python module 11/Lesson 94. MRO mixin class as context manager/robots.py
+++ Python module 11/Lesson 94. MRO mixin class as context manager/robots.py
@@ -13,4 +13,40 @@ class CanFly(Robot):
         self.speed = speed
 
     def takeoff(self):
-        print(f'Robot {self.model} is taking off...\nAltitude: {self.altitude + 50}')
+        print(f'Robot {self.model} is taking off...\nAltitude: {self.altitude + 50}\n')
+
+    def fly(self):
+        print(f'Robot {self.model} is flying...\nSpeed: {self.speed + 50}\n')
+
+    def land(self):
+
+        print(f'Robot {self.model} is landing...\nSpeed: {self.speed}\nAltitude: {self.altitude}\n')
+
+
+class SpyDrone(CanFly, Robot):
+    def __init__(self, model, altitude=0, speed=0):
+        super().__init__(model, altitude, speed)
+
+    def operate(self):
+        print(f'Robot {self.model} is looking for targets...\nSpeed: {self.speed + 50}\n')
+
+
+class BattleDrone(CanFly, Robot):
+    def __init__(self, model, altitude=0, speed=0):
+        super().__init__(model, altitude, speed)
+
+    def operate(self):
+        print(f'Robot {self.model} is defending the military object...\n')
+
+
+spy_drone = SpyDrone('Spy Drone')
+battle_drone = BattleDrone('Battle Drone')
+
+spy_drone.takeoff()
+spy_drone.fly()
+battle_drone.takeoff()
+battle_drone.fly()
+spy_drone.operate()
+battle_drone.operate()
+spy_drone.land()
+battle_drone.land()
