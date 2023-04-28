abd339eeb559e17f74bd3698d61560bcba82c8f7
CaesarMaster <dereza.niki@gmail.com>
2023-04-28 21:48:25 +0200

    updt

--- Projects/Other programs/Games/hangman.py
+++ Projects/Other programs/Games/hangman.py
@@ -1,28 +1,33 @@
-import random
+# import random
+#
+# tries = 10
+# words = ['cars', 'mouse', 'mug', 'monitor', 'alphabet']
+# chosen_word = random.choice(words)
+# length = len(chosen_word)
+# display = '_' * length
+# guessed = []
+#
+# while tries > 0:
+#     guess = input(f'This is the word: {display} Enter your letter: ')
+#     if guess in chosen_word:
+#         guessed.extend(guess)
+#         index = chosen_word.find(guess)
+#         chosen_word = chosen_word[:index] + "_" + chosen_word[index + 1:]
+#         display = display[:index] + guess + display[index + 1:]
+#         print(display + "\n")
+#
+#     elif guess in guessed:
+#         print('Try another letter.\n')
+#
+#     else:
+#         tries -= 1
+#         print(f'Tries left: {tries}')
+#
+#     if len(''.join(guessed)) == length:
+#         print('You have guessed the word!')
+#         break
 
-tries = 10
-words = ['cars', 'mouse', 'mug', 'monitor', 'alphabet']
-chosen_word = random.choice(words)
-length = len(chosen_word)
-display = '_' * length
-guessed = []
 
-while tries > 0:
-    guess = input(f'This is the word: {display} Enter your letter: ')
-    if guess in chosen_word:
-        guessed.extend(guess)
-        index = chosen_word.find(guess)
-        chosen_word = chosen_word[:index] + "_" + chosen_word[index + 1:]
-        display = display[:index] + guess + display[index + 1:]
-        print(display + "\n")
-
-    elif guess in guessed:
-        print('Try another letter.\n')
-
-    else:
-        tries -= 1
-        print(f'Tries left: {tries}')
-
-    if len(''.join(guessed)) == length:
-        print('You have guessed the word!')
-        break
+weight_lbs = int(input())
+weight_kilo = weight_lbs * 0.45
+print(f'{weight_kilo} kg')
---
+++ Projects/Software/code_with_me_test.py
@@ -0,0 +1,5 @@
+inch = float(input('what is the length (inch)? '))
+centimetre = inch * 2.54
+print(centimetre)
+
+
