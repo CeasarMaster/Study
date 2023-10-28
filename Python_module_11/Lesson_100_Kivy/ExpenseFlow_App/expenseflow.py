d40cfe0ce83b33e14b068d40df2d85de552b966f
CaesarMaster <dereza.niki@gmail.com>
2023-10-28 13:36:38 +0200

    update

--- Python_module_11/Lesson_100_Kivy/ExpenseFlow_App/expenseflow.py
+++ Python_module_11/Lesson_100_Kivy/ExpenseFlow_App/expenseflow.py
@@ -5,6 +5,7 @@ from kivy.core.window import Window
 from kivy.properties import DictProperty
 from kivy.uix.popup import Popup
 from datetime import datetime
+import sqlite3
 # Window.clearcolor = (0, 0, 0, 0.93)
  
 Builder.load_file('transactions_popup.kv')
@@ -18,20 +19,31 @@ class TransactionsScreen(Screen):
 class AccountScreen(Screen):
     pass
 class TransactionPopup(Popup):
-    def on_popup_dismiss(self):
-        current_time = datetime.now()
-        date = f'{current_time.day}/{current_time.month}/{current_time.year}'
-        time = f'{current_time.hour}:{current_time.minute}'
-        amount = self.ids.transaction_amount.text
-        category = self.ids.selected_category.text
-        print(date,time,amount,category)
     def add_transaction(self):
         current_time = datetime.now()
         date = f'{current_time.day}/{current_time.month}/{current_time.year}'
         time = f'{current_time.hour}:{current_time.minute}'
         amount = self.ids.transaction_amount.text
         category = self.ids.selected_category.text
-        print(date,time,amount,category)        
+        try:
+            sqliteConnection = sqlite3.connect('/Users/nikitadereza/PycharmProjects/Study/Python_module_11/Lesson_100_Kivy/test.db')
+            cursor = sqliteConnection.cursor()
+            print('Succesfully connected to database')
+            params = (date, time, category, amount)
+            sqlite_insert_query = """INSERT INTO new_transaction(date,time,category,amount) VALUES(?,?,?,?)"""
+            count = cursor.execute(sqlite_insert_query, params)
+            sqliteConnection.commit()
+            print('Record succesfully inserted into table', cursor.rowcount)
+            cursor.close()
+
+        except sqlite3.Error as error:
+            print('Failed to insert data into table', error)
+
+        finally:
+            if sqliteConnection:
+                sqliteConnection.close()
+                print('Connection closed')
+       
 
 class ExpenseFlow(App):
     def build(self):
@@ -50,9 +62,7 @@ class ExpenseFlow(App):
 
 
 
-
-
-    
-
+def main():
+    pass
 if __name__ == '__main__':
-    ExpenseFlow().run()
+   ExpenseFlow().run
